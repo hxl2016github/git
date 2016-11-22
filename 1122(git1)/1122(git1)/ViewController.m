3647a97a064703f4f2db4b4202f4f3f25bd8a7a7
hxl2016github <879316953@qq.com>
2016-11-22 17:24:41 +0800

    更新项目

--- 1122(git1)/1122(git1)/ViewController.m
+++ 1122(git1)/1122(git1)/ViewController.m
@@ -16,6 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSArray *a=[self.view subviews];
+    NSLog(@"%@",a);
     // Do any additional setup after loading the view, typically from a nib.
 }
 
