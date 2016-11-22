ea7bfe54db410fb67c53009004d5906f12268737
hxl2016github <879316953@qq.com>
2016-11-22 17:10:22 +0800

    新建一个工程

---
+++ 1122(git1)/1122(git1)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1122(git1)
+//
+//  Created by admin on 16/11/22.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 1122(git1)/1122(git1)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1122(git1)
+//
+//  Created by admin on 16/11/22.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
